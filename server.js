83f5685a25cad6b8af1aeded17adcaed0c7548eb
Anusha Gude <anushagude29@gmail.com>
2016-06-28 13:46:27 -0400

    update language

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var middleware = require('./middleware.js');
 app.use(middleware.logger);
 //app.use(middleware.requireAuthentication);
 app.get('/about',middleware.requireAuthentication,function(req,res){
-	res.send('About page!');
+	res.send('About us!');
 });
 app.use(express.static(__dirname + '/public'));
 //console.log(__dirname);
